6b625561976908eb6ee9b6a6845de1f9003c1d04
a.harada <redhornet96@gmail.com>
2016-11-20 15:50:35 +0900

    modify

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# import MeCab
-# import pandas as pd
+import MeCab
 import numpy as np
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
-
-# 参考サイトのコピペ
-# 文章をmecabで分かちがきして、名詞・動詞・形容詞の単語一覧を返す
-
-import MeCab
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 
 def split(text):
     tagger = MeCab.Tagger()
@@ -47,39 +46,76 @@ def parse():
 def splited_data(train):
     splited_data = []
     for datum in train:
-        splited_data.append(split(datum[0]))
+        splited_data.append(split(datum[6]))
     return splited_data
 
-
 train = np.array([
-    ['あいうえお', 1],
-    ['かきくけこ', 2],
-    ['さしすえそ', 3],
-    ['たちつてと', 4],
-    ['ほげほげお', 4],
+    [1, 0, 0, 0, 0, 0, 'こんにちは', 1],
+    [0, 1, 0, 1, 1, 0, 'パソコンが壊れました。どうすればいいですか？', 2],
+    [0, 1, 1, 0, 1, 1, 'パソコンを買いたいのですがどこで買おうかな？', 3],
 ])
+
 
 bodies = splited_data(train)
+print(bodies)
 count_vectorizer = CountVectorizer()
 feature_vectors = count_vectorizer.fit_transform(bodies)  # csr_matrix(疎行列)が返る
+feature_arr = feature_vectors.toarray()
 vocabulary = count_vectorizer.get_feature_names()
 
-print(feature_vectors)
-print(train[:,1])
+feature_learn = np.c_[train[:,0:5], feature_arr]
+feature_learn = feature_learn.astype(float)
+label_learn = train[:,-1]
 
-estimator = LogisticRegression(C=1e5)
-estimator.fit(feature_vectors, train[:,1])
+# ロジスティック回帰
+# estimator = LogisticRegression(C=1e5)
+# estimator.fit(feature_learn, label_learn)
+
+# SVM
+# svm_tuned_parameters = [
+#     {
+#         'kernel': ['rbf', 'linear'],
+#         'gamma': [2**n for n in range(-15, 3)],
+#         'C': [2**n for n in range(-5, 15)]
+#     }
+# ]
+# gscv = GridSearchCV(
+#     SVC(probability=True),
+#     svm_tuned_parameters,
+#     cv=2,
+#     n_jobs = 1,
+#     verbose = 3
+# )
+# gscv.fit(feature_learn, label_learn)
+# estimator = gscv.best_estimator_
+
+# ナイーブベイズ
+gnb = MultinomialNB()
+estimator = gnb.fit(feature_learn, label_learn)
+
+print(feature_learn)
 
 X = np.array([
-    ['あいうえお'],
-    ['テストテスト'],
-    ['さしすかきく'],
+    [1, 0, 0, 0, 0, 0, 'こんにちは'],  # => 1
+    [0, 1, 0, 0, 0, 0, 'パソコン'],  # => fail
+    [0, 1, 1, 0, 1, 0, 'パソコンを買いたい。'],  # => 3
+    [0, 1, 1, 0, 1, 1, 'パソコンを買いたい。どうすればいい？'],  # => 3
+    [0, 0, 1, 0, 1, 0, 'ラーメンが食べたい。おすすめはどこですか？'],  # => fail
+    [0, 0, 1, 0, 0, 1, 'エルメスのバッグが買いたいです'],  # => fail
+    [1, 0, 0, 0, 0, 0, 'こんばんは'],  # => fail
 ])
 
-Xtrain = splited_data(X)
+bodies = splited_data(X)
 count_vectorizer_pre = CountVectorizer(
     vocabulary=vocabulary
 )
-feature_vectors = count_vectorizer_pre.fit_transform(Xtrain)
-result = estimator.predict(feature_vectors)
+feature_vectors = count_vectorizer_pre.fit_transform(bodies)
+feature_arr_predict = feature_vectors.toarray()
+feature = np.c_[X[:,0:5], feature_arr_predict]
+feature = feature.astype(float)
+print(feature)
+result = estimator.predict(feature)
 print(result)
+
+result_proba = estimator.predict_proba(feature)
+print(result_proba)
